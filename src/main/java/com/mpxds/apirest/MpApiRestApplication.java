feff3155874e2995454f3a9c466c4ddd0b7119d2
MVPR-ASUS\MyASUS <marcus_vpr@hotmail.com>
2019-07-09 15:59:14 -0300

    Versão Inicial Projeto

---
+++ src/main/java/com/mpxds/apirest/MpApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.mpxds.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpApiRestApplication.class, args);
+	}
+
+}
